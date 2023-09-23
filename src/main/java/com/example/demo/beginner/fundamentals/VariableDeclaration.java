23ced1b0def88182a99ea15af6185e6a499792b7
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-09-24 00:46:58 +0200

    initial commit for local changes

---
+++ src/main/java/com/example/demo/beginner/fundamentals/VariableDeclaration.java
@@ -0,0 +1,12 @@
+package com.example.demo.beginner.fundamentals;
+
+public class VariableDeclaration {
+
+    public void showAge() {
+        // Declaring a local variable
+        int age = 25;
+
+        // This variable is local to the showAge function only
+        System.out.println(age);
+    }
+}
---
+++ src/test/java/com/example/demo/RandomTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class RandomTest {
+
+    @Test
+    @DisplayName("Should perform a random test")
+    void shouldPerformARandomTest() {
+
+    }
+
+}
