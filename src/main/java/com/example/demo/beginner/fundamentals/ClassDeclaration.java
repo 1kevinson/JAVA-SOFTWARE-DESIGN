d31adc1d23e0d286c318ee37049137e894d3f8eb
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-09-29 15:13:18 +0200

    Class declaration

---
+++ src/main/java/com/example/demo/beginner/fundamentals/ClassDeclaration.java
@@ -0,0 +1,19 @@
+package com.example.demo.beginner.fundamentals;
+
+public class ClassDeclaration {
+}
+
+
+class Computer {
+
+    // properties
+    String size;
+    String color;
+    int weight;
+
+    // methods
+    private void playVideo() {}
+    private void playMusic() {}
+    private void browseInternet() {}
+
+}
