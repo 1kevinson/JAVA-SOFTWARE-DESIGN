65e96a049728ec85059e77ebe864e6cd02d30be4
Kevin Kouomeu <kevinkouomeu@icloud.com>
2023-10-11 22:13:52 +0200

    :memo: Initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.beginner.fundamentals.VariableDeclaration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
